0a176d77121598b497e03b3ef54d599f10fbb7e0
ZKhal <zk23abe@herts.ac.uk>
2025-12-11 11:06:30 +0000

    add patient class

---
+++ src/Patient.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Patient {
+    public Patient(String medicalRecord, String patientFirstName, String patientSurname, String address) {
+        this.medicalRecord = medicalRecord;
+        this.patientFirstName = patientFirstName;
+        this.patientSurname = patientSurname;
+        this.address = address;
+    }
+
+    public String getMedicalRecord() {
+        return medicalRecord;
+    }
+
+    public void updateMedicalRecord(String medicalRecord) {
+        this.medicalRecord = medicalRecord;
+    }
+
+    public String getPatientFirstName() {
+        return patientFirstName;
+    }
+
+    public void setPatientFirstName(String patientFirstName) {
+        this.patientFirstName = patientFirstName;
+    }
+
+    public String getPatientSurname() {
+        return patientSurname;
+    }
+
+    public void setPatientSurname(String patientSurname) {
+        this.patientSurname = patientSurname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDetails(){
+        return String.format("Patient Details: \n firstname: %s\n surname: %s\n Address: %s", patientFirstName, patientSurname, address );
+    }
+
+    public String toString(){
+        return String.format(" ALl Patient Details: \n firstname: %s\n surname %s\n Address: %s\n Medical Record: %s", patientFirstName, patientSurname, address, medicalRecord);
+    }
+
+    private String medicalRecord;
+    private String patientFirstName;
+    private String patientSurname;
+    private String address;
+
+
+}
+

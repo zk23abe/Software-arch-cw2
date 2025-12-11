be2ec4d46d0552ee03faa2ec1cd9a0cc87c51a59
ZKhal <zk23abe@herts.ac.uk>
2025-12-11 10:33:19 +0000

    add Person class constructor and attributes

---
+++ src/Person.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Person {
+    public Person(String personId, String fullName, Date dob) {
+        this.personId = personId;
+        this.fullName = fullName;
+        this.dob = dob;
+    }
+    String personId;
+    String fullName;
+    Date dob;
+}
+
+

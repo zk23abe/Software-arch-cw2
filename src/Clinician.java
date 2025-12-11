b56180b128a85257370ccad2bc3b4f7a8304df80
ZKhal <zk23abe@herts.ac.uk>
2025-12-11 13:09:20 +0000

    fix clinician class spelling

--- src/Clinician.java
+++ src/Clinician.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
-public class Clinican extends Person {
-    public Clinican(String clinicanId, String fullName, Date dob, boolean isNurse){
+public class Clinician extends Person {
+    public Clinician(String clinicanId, String fullName, Date dob, boolean isNurse){
         super(clinicanId,fullName,dob);
         this.isNurse=isNurse;
     }

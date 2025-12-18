4ac3ccc3bdf033ab0894845f92beb3d2a2775941
ZKhal <zk23abe@herts.ac.uk>
2025-12-18 13:55:40 +0000

    simplify csvReader class

--- src/CSVReader.java
+++ src/CSVReader.java
@@ -6,39 +6,37 @@ import java.util.HashMap;
 
 public class CSVReader {
 
-    private HashMap<String, ArrayList<String>> data;
-
-    public HashMap<String, ArrayList<String>> getData(){
+    private ArrayList< ArrayList<String>> data;
+    private ArrayList<String> headers;
+    public ArrayList<String> getHeaders(){
+        return this.headers;
+    }
+    public ArrayList< ArrayList<String>> getData(){
         return data;
     }
 
     public CSVReader(String path) throws IOException {
         ArrayList<String> headersplit;
         BufferedReader bufferedReader;
-        data = new HashMap<>();
+        data = new ArrayList<>();
         try {
             FileReader file = new FileReader(path);
             bufferedReader = new BufferedReader(file);
             String headings = bufferedReader.readLine();
-            headersplit = parseLine(headings);
+            headers = parseLine(headings);
         } catch (Exception e) {
-            System.out.println(e);
+
             return;
         }
-        //HashMap<String, ArrayList<String>> data = new HashMap<String, ArrayList<String>>();
-        for (String heading : headersplit) {
-            data.put(heading, new ArrayList<String>());
-        }
+
         while(true){
             String currentLine = bufferedReader.readLine();
             if(currentLine == null){
                 break;
             }
             ArrayList<String> lineSplit = parseLine(currentLine);
-            for (int i=0; i<data.size(); i++) {
-                String heading=headersplit.get(i);
-                data.get(heading).add(lineSplit.get(i));
-            }
+
+            this.data.add(lineSplit);
         }
     }
     private ArrayList<String> parseLine(String line) throws IOException {
